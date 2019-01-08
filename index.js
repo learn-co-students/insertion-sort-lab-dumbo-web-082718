9a6c712e4f2971f89b9919bb33fcbbf106d3c4a1
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-08 12:38:53 -0500

    done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let currentMinimum = array[0]
+  let minimumIndex = 0
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] < currentMinimum){
+      currentMinimum = array[i]
+      minimumIndex = i
+    }
+  }
+  array.splice(minimumIndex, 1)
+  return currentMinimum
 }
 
 function insertionSort(array){
-
+  let sortedArray = []
+  let minimum
+  while(array.length != 0){
+    minimum = findMinAndRemove(array)
+    sortedArray.push(minimum)
+  }
+  return sortedArray
 }
